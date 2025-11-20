fc1c5c71fc8e6960e07e298f653523c8e452f090
nemuineru <suteado16x16@gmail.com>
2025-11-20 23:06:20 +0900

    Air-ride time. not doing anything rn

--- Assets/MyAsset/Resources/StateScript/Rusty/stateScript_main.mjs
+++ Assets/MyAsset/Resources/StateScript/Rusty/stateScript_main.mjs
@@ -4,16 +4,16 @@
 // and put the init value lastly.
 
 // We could use List name, if they call variable first.
-export function StateDef_0_ID(entity : any) {
+export function StateDef_0_ID(entity) {
     //List<Int>
     const List_Int =
         puer.$generic(CS.System.Collections.Generic.List$1, CS.System.Int32);    
     
     let verdList = new List_Int();
 
-    let selfOnGrd : boolean = CS.LuaCondition.isEntityOnGround(entity)
-    let isPressed_A: boolean = CS.LuaCondition.isEntityOnGround(entity, "_a");
-    let isPressed_B: boolean = CS.LuaCondition.isEntityOnGround(entity, "_b");
+    let selfOnGrd = CS.LuaCondition.isEntityOnGround(entity)
+    let isPressed_A = CS.LuaCondition.isEntityOnGround(entity, "_a");
+    let isPressed_B = CS.LuaCondition.isEntityOnGround(entity, "_b");
 
     //this must be set as 0.
     let selfStTime = CS.LuaCondition.CheckStateTime(entity) 
@@ -40,7 +40,7 @@ export function StateDef_0_ID(entity : any) {
 // not only this function, but those params need to be 
 // returned as GenericList such like List<object>,
 // ..otherwise it fails completely. - N.
-export function StateDef_0_Param(entity: any) { 
+export function StateDef_0_Param(entity) { 
     const List_Object =
         puer.$generic(CS.System.Collections.Generic.List$1, CS.System.object);
     let outs = new List_Object();
@@ -53,8 +53,8 @@ export function StateDef_0_Param(entity: any) {
     //オブジェクトのRigidBodyを取得する.
     vel3 = in_entity.rigid.velocity    
     //オブジェクトの正面方向・右方向を考え、Dotで計算.
-       let vel_relate_f = entity.transform.forward;
-       let vel_relate_r = entity.transform.right;
+    let vel_relate_f = entity.transform.forward;
+    let vel_relate_r = entity.transform.right;
     vel2.x = Vector3.Dot(vel3,vel_relate_r)
     vel2.y = Vector3.Dot(vel3,vel_relate_f)
 
