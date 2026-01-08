caea0ba82bba726544809a86776457237242d66d
nemuineru <suteado16x16@gmail.com>
2026-01-08 23:34:16 +0900

    Workin on the Animancer. This is tough..

--- Assets/MyAsset/Script/testing/AnimancerDef.cs
+++ Assets/MyAsset/Script/testing/AnimancerDef.cs
@@ -11,7 +11,7 @@ public class AnimancerDef : MonoBehaviour
         [SerializeField]
         string ClipName;
         [SerializeField]
-        AnimationClip clip;    
+        internal AnimationClip clip;    
 
         [SerializeField]
         Vector2 paramPos;
@@ -27,10 +27,31 @@ public class AnimancerDef : MonoBehaviour
     [SerializeField]
     public List<clipGroup> _clipgroup;
     
-    AnimancerState _state;
+    [SerializeField]
+    AnimancerComponent _mainAnimComponent;
 
+    //まず、StateDefで指定したAnimSlot..はLayerとして管理する.
+    //Layerの配列アクセス時、作成されていないレイヤーを指定しても例外にならないのは良いね
+
+    //で、Layer内に登録させるStatesは
+    // [Direct, Linear1D, Cartestan/Directional Freeform 2D]
+    //の4つのMixingが可能となってる.
+    
     void ChangeAnims()
     {
+        AnimancerLayer Layer1_ = _mainAnimComponent.Layers[0];
         
+        AnimancerState st = new CartesianMixerState();
+
+        //get first clipGroup.
+        foreach(clipSetting CLI in _clipgroup[0].clips)
+        {
+            ((CartesianMixerState)st).Add(CLI.clip);
+        }
+
+        Layer1_.Play(st);
+
+        //AnimancerState PlayState = new AnimancerState();
+        //Layer1_.Play(PlayState);
     }
 }
