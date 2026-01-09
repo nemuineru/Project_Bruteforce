cead53d13d23c68e09739733b18fcfc7d321a084
nemuineru <suteado16x16@gmail.com>
2026-01-09 22:21:19 +0900

    Testing about Animancer Params. it seems work rn

--- Assets/MyAsset/Script/testing/AnimancerDef.cs
+++ Assets/MyAsset/Script/testing/AnimancerDef.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using Animancer;
 using System.Collections.Generic;
+using System;
 
 //first, we ned to 
 public class AnimancerDef : MonoBehaviour
 {
+    [SerializeField]
+    List<Vector2> ParamPos;
     [System.Serializable]
     public class clipSetting
     {        
@@ -14,7 +17,7 @@ public class AnimancerDef : MonoBehaviour
         internal AnimationClip clip;    
 
         [SerializeField]
-        Vector2 paramPos;
+        internal Vector2 paramPos;
     }
 
     [System.Serializable]
@@ -22,6 +25,17 @@ public class AnimancerDef : MonoBehaviour
     {        
         [SerializeField]
         internal List<clipSetting> clips;    
+        [SerializeField]
+        internal mixType mixtype;
+		
+		public enum mixType
+		{
+			Direct,
+			Linear,
+			Simple_Directional,
+			Free_Directional,
+			Free_Cartestan
+		}
     }
     
     [SerializeField]
@@ -30,23 +44,46 @@ public class AnimancerDef : MonoBehaviour
     [SerializeField]
     AnimancerComponent _mainAnimComponent;
 
+    //自作のPlayableAPIを通したアニメ変更からAnimancerでの変更にする際、Layer/State/Clipの登録が必須
+    //Layerは作成されたときに例外処理無しで動く。
+
     //まず、StateDefで指定したAnimSlot..はLayerとして管理する.
     //Layerの配列アクセス時、作成されていないレイヤーを指定しても例外にならないのは良いね
 
     //で、Layer内に登録させるStatesは
     // [Direct, Linear1D, Cartestan/Directional Freeform 2D]
     //の4つのMixingが可能となってる.
+
+    //StateをGetTypeで取得して、その種類で挙動を変える、っていうのは可能そうね
     
+    //ミキシング実験はOK. 次はAdditiveをやってみる.
+    //[State]
+
+    void Start()
+    {
+        ChangeAnims();
+    }
+
+    void Update()
+    {
+        AnimancerLayer Layer1_ = _mainAnimComponent.Layers[0];
+        if (Layer1_ != null)
+        {
+            Type StateType = Layer1_.CurrentState.GetType();
+            //Layer1_.CurrentState.Parameter = ParamPos;
+        }
+    }
+
     void ChangeAnims()
     {
         AnimancerLayer Layer1_ = _mainAnimComponent.Layers[0];
         
-        AnimancerState st = new CartesianMixerState();
+        AnimancerState st = new DirectionalMixerState();
 
-        //get first clipGroup.
-        foreach(clipSetting CLI in _clipgroup[0].clips)
+        //get first clipGroup, then add the 2D Cartestian State position.
+        foreach (clipSetting CLI in _clipgroup[0].clips)
         {
-            ((CartesianMixerState)st).Add(CLI.clip);
+            ((LinearMixerState)st).Add(CLI.clip, CLI.paramPos.x);            
         }
 
         Layer1_.Play(st);
