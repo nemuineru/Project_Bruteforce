0d29d906ba3dbc6b71056e21ac2c09d6f938e3c8
nemuineru <suteado16x16@gmail.com>
2026-02-04 22:41:03 +0900

    oh hit props..

---
+++ Assets/MyAsset/Script/Environment/Props.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Props : MonoBehaviour
+{
+    [SerializeField]
+    clssDef hitBox;
+
+    void OnHit()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
