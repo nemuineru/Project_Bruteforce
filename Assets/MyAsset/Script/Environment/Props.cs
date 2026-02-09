9b3e2dd864eb23d2b00ce3370169d3e3f31c2e78
nemuineru <suteado16x16@gmail.com>
2026-02-09 23:02:37 +0900

    meaty.

--- Assets/MyAsset/Script/Environment/Props.cs
+++ Assets/MyAsset/Script/Environment/Props.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -10,10 +11,15 @@ public class Props : MonoBehaviour
     Rigidbody rigid;
 
     public bool isHit;
+    bool isBreak = false;
 
     public float disableTime = 0;
 
+    public GameObject onDestroyInstantiate;
+
     public float HP = 10f;
+
+    Animator animator;
 
     
 
@@ -27,11 +33,15 @@ public class Props : MonoBehaviour
         //POWER!!
         rigid.AddForceAtPosition(AddVelocity,hitPos, ForceMode.Impulse);
         disableTime += hitParam.hitStopTime.y;
+        HP -= hitParam.Damage;
     }
+    List<Renderer> renderers;
 
     // Start is called before the first frame update
     void Start()
     {
+        renderers = GetComponentsInChildren<Renderer>().ToList();
+        animator = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody>();
         hitBox.initClss(transform);
     }
@@ -49,11 +59,60 @@ public class Props : MonoBehaviour
         {
             disableTime -= 1f;
         }
-        isHit = false;
+        //Ondestroy, starts coroutine.
+        if(HP < 0)
+        {
+            if(!isBreak)
+            { 
+                isBreak = true;
+                StartCoroutine(Destroys());
+            }
+        }
+        else
+        {            
+            isHit = false;
+        }
+        AnimationSet();
+    }
+
+    void AnimationSet()
+    {
+        if(animator != null)
+        {
+            animator.SetBool("isDestroyed", HP < 0);
+        }
     }
 
     public bool isPausable()
     {
         return disableTime <= 0;
+    }
+
+    float destroTime_Current = 0f;
+    float destroTime_Max = 1f;
+
+    IEnumerator Destroys()
+    {
+        if(onDestroyInstantiate != null)
+        {
+            //Pops off.
+            GameObject instObj = Instantiate(onDestroyInstantiate,transform.position,Quaternion.identity);
+            instObj.GetComponent<Rigidbody>().velocity = Vector3.up * 5f;
+        }
+
+        //and add time
+        while( destroTime_Max > destroTime_Current)
+        {
+            destroTime_Current += Time.deltaTime;
+            if(renderers.Count > 0)
+            {
+                foreach(Renderer rend in renderers)
+                {
+                    rend.enabled = !rend.enabled;
+                }
+            }
+            yield return null;
+        }
+        Destroy(gameObject);
     }
 }
