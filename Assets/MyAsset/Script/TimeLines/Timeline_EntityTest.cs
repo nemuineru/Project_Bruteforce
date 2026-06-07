7d6631a7827d31caee1d2d51c28382c56d47303f
nemuineru <suteado16x16@gmail.com>
2026-06-07 16:15:24 +0900

    gainin knowledge about spline controller

--- Assets/MyAsset/Script/TimeLines/Timeline_EntityTest.cs
+++ Assets/MyAsset/Script/TimeLines/Timeline_EntityTest.cs
@@ -26,8 +26,30 @@ public class Timeline_EntityTest : MonoBehaviour, ITimeControl
         //throw new System.NotImplementedException();
     }
 
+    public void OnDisable()
+    {
+        if(spls != null && entity != null)
+        {
+            entity.transform.position = spls.EvaluatePosition(1.0f);
+        }
+    }
+
     public void OnControlTimeStop()
     {
+        Debug.Log("Timeline_EntityTest: OnControlTimeStop");
+        if(spls != null && entity != null)
+        {
+            entity.transform.position = spls.EvaluatePosition(1.0f);
+        }
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnControlTimeEnd()
+    {
+        if(spls != null && entity != null)
+        {
+            entity.transform.position = spls.EvaluatePosition(1.0f);
+        }
         //throw new System.NotImplementedException();
     }
 
@@ -37,18 +59,29 @@ public class Timeline_EntityTest : MonoBehaviour, ITimeControl
         if(spls != null && entity != null)
         {
             var percentage = (float)(time/_duration);
+            if(time >= _duration)
+            {
+                percentage = 1;
+            }
             entity.transform.position = spls.EvaluatePosition(percentage);
-            AnimancerComponent AM = 
+            Vector3 forward = spls.EvaluateTangent(percentage);
+            float angle = Vector3.SignedAngle(Vector3.up, spls.EvaluateUpVector(percentage), forward);
+            entity.transform.rotation = Quaternion.LookRotation(forward) * Quaternion.Euler(0, -angle, 0);
+        }
+        AnimancerComponent AM = 
             entity.animancerManager.main;
             if(AM != null)
             AM.Play(clip);
-        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        entity = GameObject.FindGameObjectWithTag(EntityName).GetComponent<Entity>();
+        GameObject fObj = GameObject.FindGameObjectWithTag(EntityName);
+        if(fObj != null)
+        {
+            entity = fObj.GetComponent<Entity>();
+        }
     }
 
     // Update is called once per frame
